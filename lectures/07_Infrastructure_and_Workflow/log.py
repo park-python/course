5284dfed7408a024e9c405dbac3db57396c4014c
Alexander Zhebrak <fata2ex@gmail.com>
2016-12-13 11:54:12 +0300

    ignore

--- lectures/07_Infrastructure_and_Workflow/log.py
+++ lectures/07_Infrastructure_and_Workflow/log.py
@@ -2,7 +2,7 @@ import logging
 
 
 logger = logging.getLogger('park')
-logger.setLevel(logging.DEBUG)  # А если поменять на WARNING?
+logger.setLevel(logging.WARNING)  # А если поменять на WARNING?
 
 lite_handler = logging.StreamHandler()
 lite_handler.setLevel(logging.DEBUG)  # А если тут?
@@ -12,23 +12,23 @@ lite_handler.setFormatter(formatter)
 
 logger.addHandler(lite_handler)
 
-# heavy_handler = logging.StreamHandler()
-# heavy_handler.setLevel(logging.ERROR)
-#
-# formatter = logging.Formatter('heavy [%(name)s] %(levelname)s: %(asctime)s - %(message)s')
-# heavy_handler.setFormatter(formatter)
-#
-# logger.addHandler(heavy_handler)
+heavy_handler = logging.StreamHandler()
+heavy_handler.setLevel(logging.ERROR)
+
+formatter = logging.Formatter('heavy [%(name)s] %(levelname)s: %(asctime)s - %(message)s')
+heavy_handler.setFormatter(formatter)
+
+logger.addHandler(heavy_handler)
 
 
-logger.debug('debug message')
-logger.info('info message')
-logger.warning('warning message')
-logger.error('error message')
-logger.critical('critical message')
+# logger.debug('debug message')
+# logger.info('info message')
+# logger.warning('warning message')
+# logger.error('error message')
+# logger.critical('critical message')
 
 
-# try:
-#     1 / 0
-# except ZeroDivisionError:
-#     logger.exception('What could go wrong?')
+try:
+    1 / 0
+except ZeroDivisionError:
+    logger.exception('What could go wrong?')
